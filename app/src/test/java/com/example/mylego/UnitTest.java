c8945cf552353e63ff8a11e045db840ce7a8d6ef
giedrys-michal <giedrys.michal@gmail.com>
2021-06-27 09:26:48 +0200

    WIP - unitTests
    WIP - unitTests

--- app/src/test/java/com/example/mylego/UnitTest.java
+++ app/src/test/java/com/example/mylego/UnitTest.java
@@ -1,14 +1,18 @@
 package com.example.mylego;
 
 import android.app.Application;
+import androidx.test.core.app.ApplicationProvider;
 import android.content.Context;
+import android.net.Uri;
 
 import com.example.mylego.database.DbManager;
 import com.example.mylego.rest.domain.BricksSingleSet;
 import com.example.mylego.ui.Utils;
+import com.example.mylego.ui.sets.SetsViewModel;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,6 +21,8 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class UnitTest {
+    private Context context = ApplicationProvider.getApplicationContext();
+
     @Test
     public void convertStringToURL() throws MalformedURLException {
         String providedURL = "https://developer.android.com/";
